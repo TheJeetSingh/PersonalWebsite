524adb010cd7f346b6c4d1531524e2e328a86baf
TheJeetSingh <4lifeinstantkarma@gmail.com>
2025-12-23 18:26:49 -0800

    feat: custom Spotify button with circular hover effect

--- components/SpotifyNowPlaying.tsx
+++ components/SpotifyNowPlaying.tsx
@@ -62,14 +62,41 @@ export default function SpotifyNowPlaying() {
           <p className="font-luckiestGuy text-black text-sm truncate">{track.name}</p>
           <p className="font-dynaPuff text-xs text-gray-700 truncate">by {track.artist}</p>
         </div>
-        <a
-          href={track.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="px-3 py-1 bg-green-500 hover:bg-green-600 text-white font-bouncy text-xs border-2 border-black shadow-[2px_2px_0_#000] transition-all"
-        >
-          ▶️
-        </a>
+        {/* Spotify button with comic speech bubble tooltip */}
+        <div className="relative group">
+          {/* Comic book speech bubble tooltip */}
+          <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none z-10">
+            <div className="relative bg-white border-4 border-black rounded-lg px-3 py-2 shadow-[3px_3px_0_#000] whitespace-nowrap">
+              <p className="font-luckiestGuy text-sm text-black text-center leading-tight">
+                Click to go to the song<br />Jeet is listening to!
+              </p>
+              {/* Speech bubble tail */}
+              <div className="absolute top-full left-1/2 -translate-x-1/2 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[12px] border-t-black"></div>
+              <div className="absolute top-full left-1/2 -translate-x-1/2 -mt-[1px] w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-t-[10px] border-t-white"></div>
+            </div>
+          </div>
+          <a
+            href={track.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex justify-center items-center p-2 rounded-md drop-shadow-xl bg-[#1ED760] text-white hover:translate-y-3 hover:rounded-full transition-all duration-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              strokeWidth="0"
+              fill="currentColor"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z"
+              ></path>
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   );
